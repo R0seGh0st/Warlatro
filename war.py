b14a7b5b9b4fe51f30eb05fc6236be52443d87df
rosesho <rose.shore20@gmail.com>
2025-10-22 15:17:52 -0400

    Initial Commit

---
+++ war.py
@@ -0,0 +1,26 @@
+from random import shuffle
+from tkinter import *
+
+#classes
+class card:
+    suit = ["Hearts", "Dimonds", "Spades", "Clubs"]
+    rank = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+
+    def __init__(self, s, r):
+        self.suit = s
+        self.rank = r
+
+    def __repr__(self):
+        return f"{self.rank} of {self.suit}"
+    
+    def __eq__(self, card):
+        if (self.rank == card.rank):
+            return True
+        else:
+            return False
+        
+    def __lt__ (self, card):
+        if (self.rank < card.rank) :
+            return True
+        else:
+            return False
